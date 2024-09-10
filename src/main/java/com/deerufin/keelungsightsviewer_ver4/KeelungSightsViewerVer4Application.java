3193532dfea3648eba0a77c1a3dc5db8a1ab0476
DEERDEER <100037208+deeveer@users.noreply.github.com>
2024-09-10 16:00:54 +0800

    Initial commit

---
+++ src/main/java/com/deerufin/keelungsightsviewer_ver4/KeelungSightsViewerVer4Application.java
@@ -0,0 +1,13 @@
+package com.deerufin.keelungsightsviewer_ver4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeelungSightsViewerVer4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeelungSightsViewerVer4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/deerufin/keelungsightsviewer_ver4/KeelungSightsViewerVer4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deerufin.keelungsightsviewer_ver4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeelungSightsViewerVer4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
